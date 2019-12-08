e6e826dafa378ecad72835329ef1c92293acf705
KimTaeWoo <twinvest@naver.com>
2019-12-09 01:22:51 +0900

    프로그래머스 h-index 문제

--- Programmers_H-index/Programmers_H-index/소스.cpp
+++ Programmers_H-index/Programmers_H-index/소스.cpp
@@ -1,61 +1,32 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
-
 int solution(vector<int> citations) {
-	int answer = 0;
-	sort(citations.begin(), citations.end());
-	for (int i = 0; i < citations.size(); ++i)
-	{
-		int hindex = i;
-		int count = 0;
-		int count2 = 0;
-		
-		for (int j = 0; j < citations.size(); ++j)
-			if (hindex <= citations[j])
-				count++;
-
-		for (int j = citations.size()-1; j >= 0; j--)
-			if (hindex >= citations[j])
-				count2++;
+	int index = 0;
+	//내림차순 정렬 7 6 4 1 1 0으로 정렬됨.
+	sort(citations.begin(), citations.end(), greater<int>());
 	
-		if (i >= count)
-		{
-			if (count2 <= i)
-			{
-				answer = i;
-				break;
-			}
-		}
-	}
-
-	bool flag = false;
-	for (int i = 0; i < citations.size(); ++i)
+	//비교를 하다가 인덱스에 해당하는 배열 값이 인덱스보다 작거나 같으면 해당 인덱스가 정답
+	while (index < citations.size())
 	{
-		if (citations.size() < citations[i])
-			flag = true;
-		else
-		{
-			flag = false;
+		if (citations[index] <= index)
 			break;
-		}
-			
+		index++;
 	}
-	if (flag) return citations.size();	
-	return answer;
+	return index;
 }
 
 
 int main()
 {
 	vector<int> v;
+	v.push_back(1);
+	v.push_back(7);
+	v.push_back(0);
+	v.push_back(1);
+	v.push_back(6);
 	v.push_back(4);
-	v.push_back(3);
-	v.push_back(3);
-	v.push_back(3);
-	v.push_back(3);
 
 	int ans = solution(v);
 	printf("%d", ans);
