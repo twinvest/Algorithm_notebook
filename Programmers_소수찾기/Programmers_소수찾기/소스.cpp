cbf4c18295e1ffeba2d621b77e28c17fd1a4771b
KimTaeWoo <twinvest@naver.com>
2019-12-09 17:34:20 +0900

    핵심아이디어는 내림차순 정렬을 실행한 다음에 2부터 가장 큰 수까지 소수인지 판별하고 소수라면 주어진 종이를 사용해 만들 수 있는지 판별하는것.

---
+++ Programmers_소수찾기/Programmers_소수찾기/소스.cpp
@@ -0,0 +1,65 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <functional>
+/*
+문제접근
+맨처음 가장 큰 수를 내림차순을 이용해서 만들어내고
+2부터 가장 큰 수 310까지 해당수가 소수인지 판별 + 주어진 종이로 만들 수 있는지 확인
+주어진 종이를 사용했는지 안했는지는 visit배열을 이용해 판별한다.
+*/
+
+using namespace std;
+bool checkNumber(int i, string numbers);
+
+int solution(string numbers) {
+	int answer = 0;
+	sort(numbers.begin(), numbers.end(), greater<int>());
+	vector<bool> temp(stoi(numbers) + 1);
+	for (int i = 2; i <= stoi(numbers); i++) 
+	{
+		if (temp[i] == false && checkNumber(i, numbers)) 
+		{
+			answer++;
+		}
+		if (temp[i] == false) 
+		{
+			for (int j = i; j <= stoi(numbers); j += i) 
+			{
+				temp[j] = true;
+			}
+		}
+	}
+	return answer;
+}
+
+bool checkNumber(int i, string numbers) 
+{
+	bool flag = false;
+	vector<bool> visit(numbers.length());
+	while (i != 0) 
+	{
+		flag = false;
+		int temp = i % 10;
+		for (int j = 0; j <= numbers.length(); j++) 
+		{
+			if (temp == numbers[j] - '0' && visit[j] == 0) 
+			{
+				flag = true;
+				visit[j] = 1;
+				break;
+			}
+		}
+		if (flag == false)
+			return false;
+		i /= 10;
+	}
+	return true;
+}
+
+int main()
+{
+	string a = "013";
+	int ans = solution(a);
+	printf("%d", ans);
+}
