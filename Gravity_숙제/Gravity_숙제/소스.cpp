5396a9079f0bac21527ac235f691f1dd4fde7cd6
KimTaeWoo <twinvest@naver.com>
2020-01-25 21:37:43 +0900

    90도 회전 코드 추가

--- Gravity_숙제/Gravity_숙제/소스.cpp
+++ Gravity_숙제/Gravity_숙제/소스.cpp
@@ -1,6 +1,18 @@
 #include<cstdio>
 #pragma warning (disable : 4996)
 int arr[10][10];
+int copy[10][10];
+void reverse(int N)
+{
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			copy[j][4 - i] = arr[i][j];
+		}
+	}
+}
+
 int main()
 {
 	int N;
@@ -12,6 +24,20 @@ int main()
 		for (int j = 0; j < J; ++j)
 			arr[i][j] = 1;
 	}
+
+	for (int i = 0; i < 2; ++i)
+		reverse(N);
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			printf("%d", copy[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n\n");
+
 
 	for (int i = 0; i < N; ++i)
 	{
@@ -34,10 +60,10 @@ int main()
 			}
 		}
 		int here = N - 1 - count;
-		for (int k = N-1; k > here; --k)
+		for (int k = N - 1; k > here; --k)
 			arr[k][i] = 1;
 	}
-	
+
 	printf("\n\n\n");
 	for (int i = 0; i < N; ++i)
 	{
