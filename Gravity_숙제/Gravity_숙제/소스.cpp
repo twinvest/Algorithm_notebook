e8d4bf2dcca56d7e6fb7d3cbfbb1934cfcc9ecfb
KimTaeWoo <twinvest@naver.com>
2020-01-25 21:09:00 +0900

    오른쪽으로 회전 후 그래비티 발생그림 출력

---
+++ Gravity_숙제/Gravity_숙제/소스.cpp
@@ -0,0 +1,51 @@
+#include<cstdio>
+#pragma warning (disable : 4996)
+int arr[10][10];
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; ++i)
+	{
+		int J;
+		scanf("%d", &J);
+		for (int j = 0; j < J; ++j)
+			arr[i][j] = 1;
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			printf("%d", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		int count = 0;
+		for (int j = 0; j < N; ++j)
+		{
+			if (arr[j][i] == 1)
+			{
+				++count;
+				arr[j][i] = 0;
+			}
+		}
+		int here = N - 1 - count;
+		for (int k = N-1; k > here; --k)
+			arr[k][i] = 1;
+	}
+	
+	printf("\n\n\n");
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+			printf("%d", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+}
